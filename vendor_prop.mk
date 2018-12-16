0df3c34516a35f4f2a3f8b357ce70600b79c2416
Unknown <nguyenhung9x2018@gmail.com>
2018-12-16 14:46:59 +0700

    X00T: Disable vendor default color mode
    * Fix warm color

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,7 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196610 \
 	ro.sf.lcd_density=480 \
 	sdm.debug.rotator_downscale=1 \
-	vendor.display.enable_default_color_mode=1
+	vendor.display.enable_default_color_mode=0
 
 # LED
 PRODUCT_PROPERTY_OVERRIDES += \
