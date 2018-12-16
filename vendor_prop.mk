51eb7ffa099ee9d89a23266dbead4c60815d6088
Unknown <nguyenhung9x2018@gmail.com>
2018-12-16 14:58:13 +0700

    X00T: Update default density to 420

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -242,3 +242,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # ZRAM disk
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.qti.config.zram=true
+	
+# Density
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=420	
